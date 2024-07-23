aae9e62e8be67a69486888a24632291c4f68d7d4
Artur Kuś <arturkus98@gmail.com>
2024-07-23 22:53:52 +0200

    init project

---
+++ src/test/java/top/arturkus/MyTimePlan/MyTimePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package top.arturkus.MyTimePlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTimePlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
