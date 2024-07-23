0532d3b1841bb55fd9567500175968d61d777d7c
Artur Kuś <arturkus98@gmail.com>
2024-07-23 23:29:02 +0200

    Step 2: Adding star object

---
+++ src/main/java/top/arturkus/MyTimePlan/entities/Star.java
@@ -0,0 +1,13 @@
+package top.arturkus.MyTimePlan.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Star {
+    private String name;
+    private long distance;
+}
